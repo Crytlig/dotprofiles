c32ddaf1d88c1c84ea4709480394b8d1f352865b
Crytlig <Crytlig@hotmail.com>
2023-01-03 21:31:19 +0100

    Updated nixconfigs

--- toolbox/nixbox/scripts/configuration.nix
+++ toolbox/nixbox/scripts/configuration.nix
@@ -23,22 +23,6 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the Plasma 5 Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-
-  ## LOCALE
-
-  i18n.extraLocaleSettings = {
-    LC_ALL = "en_US.UTF8";
-    LANG = "en_US.UTF8";
-    LC_MESSAGES = "en_US.UTF-8";
-    LC_TIME = "en_DK.UTF-8";
-  };
-
   # Enable DBus
   services.dbus.enable    = true;
 
@@ -55,7 +39,6 @@
     netcat
     nfs-utils
     rsync
-    vim
     git
   ];
 
@@ -71,8 +54,8 @@
     group           = "vagrant";
     extraGroups     = [ "users" "wheel" ];
     password        = "vagrant";
-    home            = "/home/vagrant";
-    createHome      = true;
+    # home            = "/home/vagrant";
+    createHome      = false;
     useDefaultShell = true;
     openssh.authorizedKeys.keys = [
           "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key"
