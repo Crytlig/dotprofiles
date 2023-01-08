dfe5d3f3902ed8e3a2737b491c3534b26f9c2067
Crytlig <Crytlig@hotmail.com>
2023-01-08 17:31:15 +0100

    Working version

--- toolbox/nixbox/scripts/configuration.nix
+++ toolbox/nixbox/scripts/configuration.nix
@@ -7,6 +7,7 @@
       ./hardware-builder.nix
       ./vagrant.nix
       ./custom-configuration.nix
+      ./plasma5.nix
     ];
 
   # Use the GRUB 2 boot loader.
@@ -53,8 +54,9 @@
     group           = "vagrant";
     extraGroups     = [ "users" "wheel" ];
     password        = "vagrant";
-    # home            = "/home/vagrant";
-    createHome      = false;
+    home            = "/home/vagrant";
+    shell           = pkgs.zsh;
+    createHome      = true;
     useDefaultShell = true;
     openssh.authorizedKeys.keys = [
           "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key"
