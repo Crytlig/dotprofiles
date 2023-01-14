8380ad6abe7b9b3161bb6e342a4b34a6ca518ae3
Crytlig <Crytlig@hotmail.com>
2023-01-14 14:31:14 +0100

    Added shebang on script

--- nixbox/scripts/additional-installs.sh
+++ nixbox/scripts/additional-installs.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 # zsh autosuggestions and syntax highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
